1e8e7038996c266d23fb14d6d34ca6934b17d265
sverrekaldestad <104861156+sverrekaldestad@users.noreply.github.com>
2024-10-29 18:35:28 +0100

    Task 3.1 finito

--- DAT101-main/module_3/task_3-1/task_3-1.mjs
+++ DAT101-main/module_3/task_3-1/task_3-1.mjs
@@ -6,38 +6,78 @@ printOut("--- Part 1, 2, 3 -----------------------------------------------------
 /* Put your code below here!*/
 printOut("Task 1, 2 and 3");
 let wakeUpTime = 2;
-if ("Replace this") {
+if (wakeUpTime == 7) {
   printOut("I can take the bus to school.");
+} else if(wakeUpTime == 8){
+  printOut("I can take the train to school")
+}else {
+  printOut("I have to take the car to school");
 }
-printOut(newLine);
+
 
 printOut("--- Part 4, 5 --------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
+let value = -17;
+
+if (value > 0){
+  printOut("Positive")
+} else if(value < 0) {
+  printOut("Negative")
+} else if(value == 0){
+  printOut("Zero")
+}
+printOut(newLine);
+
+printOut("--- Part 6 and 7 ----------------------------------------------------------------------------------------------");
+/* Put your code below here!*/
+let photoSize = 4;
+let maxSize = 6;
+let randomSize = Math.random() * 8;
+
+if (randomSize < photoSize){
+  printOut("Image is to small")
+} else if (randomSize >= maxSize){
+  printOut("Image is too large")
+} else {
+  printOut("Thank you")
+}
 printOut("Replace this with you answer!");
 printOut(newLine);
 
-printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
 
-printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+printOut("--- Part 8 and 9 ----------------------------------------------------------------------------------------------");
+const monthList = ["January", "February", "Mars", "April", "Mai", "Jun", "Juli", "August", "September", "October", "November", "December"];
+const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]; 
+const noOfMonth = monthList.length;
+const monthIndex = Math.floor(Math.random() * noOfMonth);
+const monthName = monthList[monthIndex];
+const days = daysInMonth[monthIndex];
 
-printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+printOut(`Month: ${monthName}`);
+if (monthName.includes("r")) {
+    printOut(`${monthName}: You must take vitamin D`);
+} else {
+    printOut(`${monthName}: You do not need to take vitamin D`);
+}
 
-printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+
+printOut(`There are ${days} days in ${monthName}.`);
+
+
+
 
 /* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+let status;
+
+if (monthName === "Mars" || monthName === "Mai") {
+  status = "The Gallery is closed";
+} else if (monthName === "April") {
+  status = "The Gallery has temporary premises in the building next door";
+} else {
+  status = "The Gallery is open!";
+}
+
+printOut(`Month: ${monthName}`);
+printOut(status)
 printOut(newLine);
